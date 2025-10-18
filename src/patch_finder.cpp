229ae72325ddcb72e7faf46d6ef9a6ef234441ba
momo5502 <mauriceheumann@gmail.com>
2025-10-18 13:24:56 +0200

    Parse PE files on disk

--- src/patch_finder.cpp
+++ src/patch_finder.cpp
@@ -1,24 +1,98 @@
 #include "patch_finder.hpp"
+
+#include <fstream>
+#include <cinttypes>
+#include <streambuf>
+#include <filesystem>
+
+#include "pe_parser.hpp"
+
 #include "ida_sdk.hpp"
 
 namespace momo
 {
     namespace
     {
+        qvector<modinfo_t> get_loaded_modules()
+        {
+            qvector<modinfo_t> modules;
 
+            modinfo_t modinfo{};
+            bool ok = get_first_module(&modinfo);
+
+            while (ok)
+            {
+                modules.push_back(modinfo);
+                ok = get_next_module(&modinfo);
+            }
+
+            return modules;
+        }
+
+        std::string read_module(const std::filesystem::path& module_path)
+        {
+            std::ifstream stream(module_path, std::ios::binary);
+            if (!stream)
+            {
+                return {};
+            }
+
+            return std::string(std::istreambuf_iterator<char>(stream), std::istreambuf_iterator<char>());
+        }
+
+        std::string read_module(const modinfo_t& modinfo)
+        {
+            std::string_view mod_name(modinfo.name.c_str(), modinfo.name.size());
+            return read_module(mod_name);
+        }
+
+        utils::safe_buffer_accessor<const std::byte> make_accessor(const std::string_view data)
+        {
+            std::span view(reinterpret_cast<const std::byte*>(data.data()), data.size());
+            return {view};
+        }
+
+        void find_patches_in_section(const section_map::value_type& section)
+        {
+get_fileregion_ea()
+            
+            
+            msg("Module: %s at 0x%" PRIX64 " (size: %d)\n", module.name.c_str(), module.base, module.size);
+        }
+
+        void find_patches_in_module(const modinfo_t& modinfo)
+        {
+            const auto data = read_module(modinfo);
+            const auto buffer = make_accessor(data);
+            const auto sections = parse_pe_file(buffer, modinfo.base);
+
+            for (const auto& section : sections)
+            {
+                find_patches_in_section(section);
+            }
+        }
     }
 
     void find_patches()
     {
         msg("Hello from plugin\n");
 
-        if (dbg)
-        {
-            msg("Debugger is active\n");
-        }
-        else
+        if (!is_debugger_on())
         {
             msg("Debugger is not active\n");
+            return;
+        }
+
+        for (const auto& module : get_loaded_modules())
+        {
+            try
+            {
+                find_patches_in_module(module);
+            }
+            catch (...)
+            {
+                // Just ignore all issues
+            }
         }
     }
 }
---
+++ src/pe_parser.hpp
@@ -0,0 +1,209 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "win_pefile.hpp"
+#include "buffer_accessor.hpp"
+
+namespace momo
+{
+    using section_data = std::vector<uint8_t>;
+    using section_map = std::map<uint64_t, section_data>;
+
+    namespace detail
+    {
+        template <typename SpanElement>
+        utils::safe_object_accessor<PEDosHeader_t, SpanElement> get_dos_header(const utils::safe_buffer_accessor<SpanElement>& buffer)
+        {
+            return buffer.template as<PEDosHeader_t>(0);
+        }
+
+        template <typename AddrType, typename SpanElement>
+        utils::safe_object_accessor<PENTHeaders_t<AddrType>, SpanElement> get_nt_headers(
+            const utils::safe_buffer_accessor<SpanElement>& buffer)
+        {
+            const auto dos_header = get_dos_header(buffer).get();
+            const auto nt_headers_offset = dos_header.e_lfanew;
+
+            return buffer.template as<PENTHeaders_t<AddrType>>(nt_headers_offset);
+        }
+
+        // TODO: Fix
+        template <typename AddrType>
+        uint64_t get_first_section_offset(const PENTHeaders_t<AddrType>& nt_headers, const uint64_t nt_headers_offset)
+        {
+            const auto* nt_headers_addr = reinterpret_cast<const uint8_t*>(&nt_headers);
+            const size_t optional_header_offset =
+                reinterpret_cast<uintptr_t>(&(nt_headers.OptionalHeader)) - reinterpret_cast<uintptr_t>(&nt_headers);
+            const size_t optional_header_size = nt_headers.FileHeader.SizeOfOptionalHeader;
+            const auto* first_section_addr = nt_headers_addr + optional_header_offset + optional_header_size;
+
+            const auto first_section_absolute = reinterpret_cast<uint64_t>(first_section_addr);
+            const auto absolute_base = reinterpret_cast<uint64_t>(&nt_headers);
+            return nt_headers_offset + (first_section_absolute - absolute_base);
+        }
+
+        template <typename AddrType, typename SpanElement>
+        section_map parse_sections(const utils::safe_buffer_accessor<SpanElement> buffer, const PENTHeaders_t<AddrType>& nt_headers,
+                                   const uint64_t nt_headers_offset, const uint64_t base_address)
+        {
+            section_map result{};
+
+            const auto first_section_offset = get_first_section_offset(nt_headers, nt_headers_offset);
+            const auto sections = buffer.template as<IMAGE_SECTION_HEADER>(static_cast<size_t>(first_section_offset));
+
+            for (size_t i = 0; i < nt_headers.FileHeader.NumberOfSections; ++i)
+            {
+                const auto section = sections.get(i);
+
+                if (section.SizeOfRawData <= 0 || !(section.Characteristics & IMAGE_SCN_MEM_EXECUTE))
+                {
+                    continue;
+                }
+
+                const auto target_ptr = base_address + section.VirtualAddress;
+
+                const auto size_of_data = std::min(section.SizeOfRawData, section.Misc.VirtualSize);
+                const auto* byte_ptr = buffer.get_pointer_for_range(section.PointerToRawData, size_of_data);
+                const auto* source_ptr = reinterpret_cast<const uint8_t*>(byte_ptr);
+
+                section_data data{};
+                data.assign(source_ptr, source_ptr + size_of_data);
+
+                result[target_ptr] = std::move(data);
+            }
+
+            return result;
+        }
+
+        inline section_map::iterator find_section(section_map& sections, const uint64_t address)
+        {
+            auto iter = sections.upper_bound(address);
+            if (iter == sections.begin())
+            {
+                return sections.end();
+            }
+
+            std::advance(iter, -1);
+            return iter;
+        }
+
+        template <typename T>
+            requires(std::is_integral_v<T>)
+        bool apply_relocation(section_map& sections, const uint64_t address, const uint64_t delta)
+        {
+            auto section = find_section(sections, address);
+            if (section == sections.end())
+            {
+                return false;
+            }
+
+            utils::safe_buffer_accessor<uint8_t> buffer{section->second};
+
+            const auto offset = address - section->first;
+            const auto obj = buffer.template as<T>(static_cast<size_t>(offset));
+            const auto value = obj.get();
+            const auto new_value = value + static_cast<T>(delta);
+            obj.set(new_value);
+
+            return true;
+        }
+
+        template <typename AddrType, typename SpanElement>
+        void apply_relocations(const utils::safe_buffer_accessor<SpanElement> buffer, const PEOptionalHeader_t<AddrType>& optional_header,
+                               section_map& sections, const int64_t delta, const uint64_t base_address)
+        {
+            if (delta == 0)
+            {
+                return;
+            }
+
+            const auto* directory = &optional_header.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC];
+            if (directory->Size == 0)
+            {
+                return;
+            }
+
+            auto relocation_offset = directory->VirtualAddress;
+            const auto relocation_end = relocation_offset + directory->Size;
+
+            while (relocation_offset < relocation_end)
+            {
+                const auto relocation = buffer.template as<IMAGE_BASE_RELOCATION>(relocation_offset).get();
+
+                if (relocation.VirtualAddress <= 0 || relocation.SizeOfBlock <= sizeof(IMAGE_BASE_RELOCATION))
+                {
+                    break;
+                }
+
+                const auto data_size = relocation.SizeOfBlock - sizeof(IMAGE_BASE_RELOCATION);
+                const auto entry_count = data_size / sizeof(uint16_t);
+
+                const auto entries = buffer.template as<uint16_t>(relocation_offset + sizeof(IMAGE_BASE_RELOCATION));
+
+                relocation_offset += relocation.SizeOfBlock;
+
+                for (size_t i = 0; i < entry_count; ++i)
+                {
+                    const auto entry = entries.get(i);
+
+                    const int type = entry >> 12;
+                    const auto offset = static_cast<uint16_t>(entry & 0xfff);
+                    const auto address = base_address + relocation.VirtualAddress + offset;
+
+                    switch (type)
+                    {
+                    case IMAGE_REL_BASED_ABSOLUTE:
+                        break;
+
+                    case IMAGE_REL_BASED_HIGHLOW:
+                        apply_relocation<uint32_t>(sections, address, delta);
+                        break;
+
+                    case IMAGE_REL_BASED_DIR64:
+                        apply_relocation<uint64_t>(sections, address, delta);
+                        break;
+
+                    default:
+                        throw std::runtime_error("Unknown relocation type: " + std::to_string(type));
+                    }
+                }
+            }
+        }
+
+        template <typename AddrType, typename SpanElement>
+        section_map parse_pe_variant(const utils::safe_buffer_accessor<SpanElement>& buffer, const uint64_t base_address)
+        {
+            const auto dos_header = get_dos_header(buffer).get();
+            const auto nt_headers_offset = dos_header.e_lfanew;
+            const auto nt_headers = get_nt_headers<AddrType>(buffer).get();
+            const auto& optional_header = nt_headers.OptionalHeader;
+
+            const int64_t aslr_slide = base_address - optional_header.ImageBase;
+
+            auto sections = parse_sections(buffer, nt_headers, nt_headers_offset, base_address);
+            apply_relocations(buffer, optional_header, sections, aslr_slide, base_address);
+
+            return sections;
+        }
+    }
+
+    template <typename SpanElement>
+    section_map parse_pe_file(const utils::safe_buffer_accessor<SpanElement>& buffer, const uint64_t base_address)
+    {
+        const auto nt_headers = detail::get_nt_headers<uint64_t>(buffer);
+        const auto machine_type = nt_headers.get().FileHeader.Machine;
+
+        switch (machine_type)
+        {
+        case PEMachineType::I386:
+            return detail::parse_pe_variant<uint32_t>(buffer, base_address);
+        case PEMachineType::AMD64:
+            return detail::parse_pe_variant<uint64_t>(buffer, base_address);
+        default:
+            return {};
+        }
+    }
+}
